49ececc1c2c6a93eb0cace8908c78d8dfb9a9b1e
Dhinesh63 <60658107+Dhinesh63@users.noreply.github.com>
2020-06-15 14:11:56 +0530

    Add files via upload

---
+++ Language basics/Sample2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Sample2{
+public static void main(String args[]){
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+
+System.out.println("The sum of "+a+" and "+b+" is "+(a+b));
+
+}}
